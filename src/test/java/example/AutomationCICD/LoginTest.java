9b5454124865e9186f8fcbf14cefeed517637a90
satya7288 <nvvsn9988@gmail.com>
2026-02-12 22:10:45 +0530

    first commit

---
+++ src/test/java/example/AutomationCICD/LoginTest.java
@@ -0,0 +1,51 @@
+package example.AutomationCICD;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setup() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");  
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    void testValidLogin() {
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name"))
+                .sendKeys("standard_user");
+
+        driver.findElement(By.id("password"))
+                .sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button"))
+                .click();
+
+        String currentUrl = driver.getCurrentUrl();
+
+        assertTrue(currentUrl.contains("inventory"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+}
